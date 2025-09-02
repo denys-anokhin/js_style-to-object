02dec66bbca9510195366193a703bfea2e4abee0
Denys Anokhin <denys.anokhin.dev@gmail.com>
2025-09-02 23:01:12 +0300

    js_style-to-object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const objectString = {};
+  const arrayString = sourceString
+    .split(';')
+    .filter((value) => value.trim().length > 1)
+    .map((value) => value.split(':'));
+
+  arrayString.forEach(([a, b]) => {
+    objectString[
+      a
+        .trim()
+        .split('')
+        .filter((letter) => letter !== '\n')
+        .join('')
+    ] = b.trim();
+  });
+
+  return objectString;
 }
 
 module.exports = convertToObject;
